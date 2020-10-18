02a23f7b6457c6ed15243108596d720d3379ecdd
Anggra Dinanda <anggradinanda632gmail.com>
2020-10-18 22:19:20 +0700

    Commit pertama

---
+++ app/src/main/java/com/example/tugas1_1818005/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tugas1_1818005;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
